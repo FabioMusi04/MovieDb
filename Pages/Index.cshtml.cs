4cf4351eb20fb6e18a9a5038a6f6a7d0ae460974
FabioMusi04 <76118020+FabioMusi04@users.noreply.github.com>
2022-12-16 19:52:26 +0100

    Add files via upload

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MoviesDb.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
 
 namespace MoviesDb.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly IConfiguration configuration;
+        public IndexModel(IConfiguration configuration)
         {
-            _logger = logger;
+            this.configuration = configuration;
         }
 
-        public void OnGet()
+        [BindProperty]
+        public string UserName { get; set; }
+        [BindProperty, DataType(DataType.Password)]
+        public string Password { get; set; }
+        public string Message { get; set; }
+        public async Task<IActionResult> OnPost()
         {
+            var user = configuration.GetSection("Account").Get<AccountUser>();
 
+            if (UserName == user.UserName)
+            {
+                var passwordHasher = new PasswordHasher<string>();
+                if (passwordHasher.VerifyHashedPassword(null, user.Password, Password) == PasswordVerificationResult.Success)
+                {
+                    var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, UserName)
+                    };
+                    var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+                    return RedirectToPage("/Movies/Index");
+                }
+            }
+            Message = "Invalid attempt";
+            return Page();
         }
     }
 }
